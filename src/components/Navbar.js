9cdd7f0e3ac113499bbcad6769124f58a808c971
jimmyov29 <jimmy.ov2998@gmail.com>
2023-05-30 23:35:38 -0600

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className=' navbar navbar-dark bg-dark'>
+        <div className=' container'>
+            <a className='navbar-brand' href='/'>React Redux App</a>
+        </div>
+    </nav>
+  )
+}
+export default Navbar;
