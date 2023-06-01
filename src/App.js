6f635482ed2701aa3dfb4b4c369cd7e114a97392
jimmyov29 <jimmy.ov2998@gmail.com>
2023-05-31 23:55:54 -0600

    usersSlices done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import UserList from "./components/UserList";
-
+//redux
+import {Provider} from 'react-redux'
+import store from "./store";
 function App() {
   return (
-    <>
+    <Provider store={store}>
     <Navbar/>
     <UserList/>
 
-    </>
+    </Provider>
     
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
+//reducer
+import users from './slices/users';
 
 export default configureStore({
     reducer: {
-
-
+        users
     }
-})
+});
