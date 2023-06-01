1b1b3491baa33efab9260013c0689ebe338119c9
jimmyov29 <jimmy.ov2998@gmail.com>
2023-06-01 00:34:48 -0600

    final commit

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,49 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+//redux
+import { fetchAllUsers } from "../store/slices/users";
+import { useDispatch, useSelector } from "react-redux";
 
 export const UserList = () => {
-    const users = [
-        {
-      "id": 7,
-      "email": "michael.lawson@reqres.in",
-      "first_name": "Michael",
-      "last_name": "Lawson",
-      "avatar": "https://reqres.in/img/faces/7-image.jpg"
-    },
-    {
-      "id": 8,
-      "email": "lindsay.ferguson@reqres.in",
-      "first_name": "Lindsay",
-      "last_name": "Ferguson",
-      "avatar": "https://reqres.in/img/faces/8-image.jpg"
-    },
-    {
-      "id": 9,
-      "email": "tobias.funke@reqres.in",
-      "first_name": "Tobias",
-      "last_name": "Funke",
-      "avatar": "https://reqres.in/img/faces/9-image.jpg"
-    }
-    ]
-  return (
-    <div className='container'>
-        <div className='row'>
-            {
-                users.map((user, index)=> (
-                    <div  key={index} className='col-md-3'>
-                    <div className='card'>
-                        <img src= {user.avatar} alt='avatar'/>
-                        <div className='card-body'>
-                            <h5 className='card-title'> {`${user.first_name} ${user.last_name}`}</h5>
-                            <p className='card-text'>{user.email}</p>
-                        </div>
-                    </div>
 
-                    </div>
-                ))
-            }
-        </div>
-    
+  const {list: users } = useSelector(state => state.users);   
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(fetchAllUsers());
+  }, [dispatch])
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        {users.map((user, index) => (
+          <div key={index} className="col-md-3 mb-4">
+            <div className="card">
+              <img src={user.avatar} alt="avatar" />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {" "}
+                  {`${user.first_name} ${user.last_name}`}
+                </h5>
+                <p className="card-text">{user.email}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 export default UserList;
